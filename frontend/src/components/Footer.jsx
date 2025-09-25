d792ef155eee45f1f04bea9fcadf99bb44b4be02
Ducanh99 tỷ <ducanh1712004@gmail.com>
2025-09-25 20:35:01 +0700

    header,footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <div class="container grid">
+        <div class="footer-box">
+          <img src="./img/logo.jpg" alt="" />
+          <p>
+            Shop thời trang thể thao, cam kết mang đến những sản phẩm "Đẹp -
+            Chất - Giá hợp lý". Với chính sách bán hàng lấy khách hàng làm trung
+            tâm, uy tín là tôn chỉ hoạt động của shop. Hy vọng khách hàng sẽ cảm
+            thấy hài lòng và có những trải nghiệm tốt nhất!
+          </p>
+        </div>
+
+        <div class="footer-box">
+          <h3>HỖ TRỢ KHÁCH HÀNG</h3>
+          <ul>
+            <li>
+              <a href="#">Trang chủ</a>
+            </li>
+            <li>
+              <a href="#">Cửa hàng</a>
+            </li>
+            <li>
+              <a href="#">Tin tức</a>
+            </li>
+            <li>
+              <a href="#">Giới thiệu</a>
+            </li>
+            <li>
+              <a href="#">Liên hệ</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="footer-box">
+          <h3>Liên Kết Xã Hội</h3>
+          <div class="footer-icon">
+            <div class="icon">
+              <i class="fa-brands fa-facebook"></i>
+            </div>
+            <div class="icon">
+              <i class="fa-brands fa-tiktok"></i>
+            </div>
+            <div class="icon">
+              <i class="fa-brands fa-instagram"></i>
+            </div>
+            <div class="icon">
+              <i class="fa-brands fa-youtube"></i>
+            </div>
+          </div>
+          <div class="more">
+            <a href="https://zalo.me/0387873303" class="btn-zalo">
+              <i class="fa fa-comment-dots"></i>
+              CHAT ZALO
+            </a>
+
+            <div class="phone">
+              <i class="fa fa-phone"></i>
+              <span>0387873303</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,48 @@
+import { useNavigate, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "../styles/Header.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+
+  return (
+    <header class="header">
+      \
+      <NavLink to="/home" class="logo">
+        <img src="logo.jpg" alt="" />
+      </NavLink>
+      <div class="search-bar">
+        <i class="fa fa-search"></i>
+        <input type="text" placeholder="Tìm sản phẩm..." />
+      </div>
+      <div class="header-right">
+        <div class="consult">
+          <i class="fa fa-paper-plane"></i>
+          <span>
+            Tư vấn mua hàng<b>0387873303</b>
+          </span>
+        </div>
+        <div class="icons">
+          <div class="icon">
+            <i class="fa fa-store"></i>
+          </div>
+          <h3>{currentUser.fullname}</h3>
+          <div class="icon" onClick={handleSubmit}>
+            <i class="fa fa-user"></i>
+          </div>
+          <div class="icon">
+            <i class="fa fa-shopping-bag"></i>
+            <span class="badge">0</span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
