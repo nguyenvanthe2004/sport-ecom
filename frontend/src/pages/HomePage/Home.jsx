44b26e3ecc6dd6a91f101ecdbd71fb53a9ef816c
Ducanh99 tỷ <ducanh1712004@gmail.com>
2025-10-12 23:50:42 +0700

    mGiaodienSP,GiaodienDanhmuc

--- frontend/src/pages/HomePage/Home.jsx
+++ frontend/src/pages/HomePage/Home.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const currentUser = useSelector(state => state.auth.currentUser)
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        navigate('/login');
-    }
-    return (
-        <div>
-            <h1>Welcome to the {currentUser.email}</h1>
-            <button onClick={handleSubmit}>Login</button>
-        </div>
-    )
-}
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/login");
+  };
+  return (
+    <div>
+      <h1>welcome to the {currentUser.email}</h1>
+      <button onClick={handleSubmit}>Login</button>
+    </div>
+  );
+};
 
 export default Home;
