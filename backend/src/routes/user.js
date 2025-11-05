98ba3b770555f49f28e1a14397405e2d02775bf5
ngogiap <ngogiap1004@gmail.com>
2025-11-05 22:22:39 +0700

    CRUD user

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -7,5 +7,8 @@ router.get("/getAll", verifyUser, verifyAdmin, UserController.getAllUsers);
 router.post("/login", UserController.login);
 router.post("/register", UserController.register);
 router.get("/current",verifyUser, UserController.current);
+router.put("/edit", verifyUser, UserController.editUser);
+router.put("/changePassword", verifyUser, UserController.changePassword);
+router.delete("/remove", verifyUser, verifyAdmin, UserController.removeUser);
 
 module.exports = router;
