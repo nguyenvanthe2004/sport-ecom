aa32b27409bf5b5823e74dd6b23ba9f2b50578fe
The Nguyen <thend03122004@gmail.com>
2025-11-09 21:37:35 +0700

    update

--- frontend/src/pages/AuthPage/index.jsx
+++ frontend/src/pages/AuthPage/index.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import Login from "../../components/Login";
 import Register from "../../components/Register";
-import "../styles/AuthPage.css";
-
+import "../../styles/AuthPage.css"
 const AuthPage = () => {
   const [showLogin, setShowLogin] = useState(true);
 
